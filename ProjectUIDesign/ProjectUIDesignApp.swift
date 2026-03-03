3b74ffa2f54dc625cbbf3b527e8ac933732a794c
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-03 13:47:53 -0500

    Initial Commit

---
+++ ProjectUIDesign/ProjectUIDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectUIDesignApp.swift
+//  ProjectUIDesign
+//
+//  Created by Penny Ahlstrom on 2026-03-03.
+//
+
+import SwiftUI
+
+@main
+struct ProjectUIDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
