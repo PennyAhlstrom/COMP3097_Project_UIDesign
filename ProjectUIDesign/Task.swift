e7255c6d2d4e3e9e13da30d5ded562fef4b40d66
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-04 12:10:27 -0500

    added helper to build date for sample tasks and added reference to hardcoded course ids

--- ProjectUIDesign/Task.swift
+++ ProjectUIDesign/Task.swift
@@ -51,40 +51,43 @@ struct Task: Identifiable, Hashable {
     }
 }
 
-//extension Task {
-//    static let sampleTasks: [Task] = [
-//        .init(
-//            courseID: ,
-//            title: "LabEx1",
-//            type: "LAB",
-//            dueDate: 2026-02-23,
-//            isCompleted: true,
-//            isBonus: false,
-//            isPriority: false,
-//            weight: 1.5,
-//            scorePercent: 100
-//        ),
-//        .init(
-//            courseID: ,
-//            title: "Assignment 1",
-//            type: "ASSIGNMENT",
-//            dueDate: "2026-02-23",
-//            isCompleted: true,
-//            isBonus: false,
-//            isPriority: false,
-//            weight: 15,
-//            scorePercent: 100
-//        ),
-//        .init(
-//            courseID: ,
-//            title: "Quiz Week 1",
-//            type: "QUIZ",
-//            dueDate: "2026-02-23",
-//            isCompleted: false,
-//            isBonus: false,
-//            isPriority: true,
-//            weight: 2,
-//            scorePercent: 0
-//        )
-//    ]
-//}
+extension Task {
+    private static func ymd(_ year: Int, _ month: Int, _ day: Int) -> Date {
+            Calendar.current.date(from: DateComponents(year: year, month: month, day: day))!
+        }
+    static let sampleTasks: [Task] = [
+        .init(
+            courseID: Course.SampleIDs.course1,
+            title: "LabEx1",
+            type: "LAB",
+            dueDate: ymd(2026, 2, 03),
+            isCompleted: true,
+            isBonus: false,
+            isPriority: false,
+            weight: 1.5,
+            scorePercent: 100
+        ),
+        .init(
+            courseID: Course.SampleIDs.course1,
+            title: "Assignment 1",
+            type: "ASSIGNMENT",
+            dueDate: ymd(2026, 2, 23),
+            isCompleted: true,
+            isBonus: false,
+            isPriority: false,
+            weight: 15,
+            scorePercent: 100
+        ),
+        .init(
+            courseID: Course.SampleIDs.course1,
+            title: "Quiz Week 1",
+            type: "QUIZ",
+            dueDate: ymd(2026, 2, 08),
+            isCompleted: false,
+            isBonus: false,
+            isPriority: true,
+            weight: 2,
+            scorePercent: 0
+        )
+    ]
+}
