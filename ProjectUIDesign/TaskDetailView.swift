f08e22df0fb4b8aedfa25dd7b180b31f236e7c11
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-04 13:16:08 -0500

    added due date iso formatting

--- ProjectUIDesign/TaskDetailView.swift
+++ ProjectUIDesign/TaskDetailView.swift
@@ -5,34 +5,6 @@
 //  Created by Penny Ahlstrom on 2026-03-03.
 //
 
-//import SwiftUI
-//
-//struct TaskDetailView: View {
-//    let task: Task
-//
-//    var body: some View {
-//        List {
-//            Section("Task") {
-//                DetailRow(label: "Title", value: task.title)
-//                DetailRow(label: "Type", value: task.type)
-//                DetailRow(label: "Due Date", value: task.dueDate)
-//            }
-//
-//            Section("Status") {
-//                DetailRow(label: "Completed", value: task.isCompleted ? "Yes" : "No")
-//                DetailRow(label: "Priority", value: task.isPriority ? "Yes" : "No")
-//                DetailRow(label: "Bonus", value: task.isBonus ? "Yes" : "No")
-//            }
-//
-//            Section("Grading") {
-//                DetailRow(label: "Weight", value: "\(task.weight, default: "%.1f")%")
-//                DetailRow(label: "Score", value: "\(task.scorePercent, default: "%.0f")%")
-//            }
-//        }
-//        .navigationTitle("Task")
-//    }
-//}
-
 import SwiftUI
 
 struct TaskDetailView: View {
@@ -51,7 +23,8 @@ struct TaskDetailView: View {
                     Section("Task") {
                         DetailRow(label: "Title", value: task.title)
                         DetailRow(label: "Type", value: task.type)
-                        DetailRow(label: "Due Date", value: task.dueDate)
+                        DetailRow(label: "Due Date", value: task.dueDate?.formatted(.iso8601.year().month().day()) ?? "No due date"
+                        )
                     }
 
                     Section("Status") {
