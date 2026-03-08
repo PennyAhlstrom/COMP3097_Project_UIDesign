1c53dcc124c025c7ec51763ca247cad5fb590ecc
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-08 12:14:20 -0400

    added color theme file

---
+++ ProjectUIDesign/Extensions/Color+Theme.swift
@@ -0,0 +1,19 @@
+//
+//  Color+Theme.swift
+//  ProjectUIDesign
+//
+//  Created by Penny Ahlstrom on 2026-03-08.
+//
+
+import SwiftUI
+
+extension Color {
+
+    static let coursesBackground = Color.blue.opacity(0.1)
+
+    static let tasksBackground = Color.green.opacity(0.1)
+
+    static let progressBackground = Color.orange.opacity(0.1)
+
+    static let remindersBackground = Color.purple.opacity(0.1)
+}
